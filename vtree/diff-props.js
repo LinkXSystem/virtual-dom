d5a0f8fef6b33b1501c25146c9f8b9b585f3799d
Zhi Yang <zhi@dankal.cn>
2019-01-07 23:10:42 +0800

    📝 writing annotation about reading code

--- vtree/diff-props.js
+++ vtree/diff-props.js
@@ -1,58 +1,61 @@
-var isObject = require("is-object")
-var isHook = require("../vnode/is-vhook")
+var isObject = require('is-object');
+var isHook = require('../vnode/is-vhook');
 
-module.exports = diffProps
+module.exports = diffProps;
 
 function diffProps(a, b) {
-    var diff
-
+    var diff;
+    // 判断 两个 node 的属性是否一直，否则将差异的部分合并到 diff 中
     for (var aKey in a) {
         if (!(aKey in b)) {
-            diff = diff || {}
-            diff[aKey] = undefined
+            diff = diff || {};
+            diff[aKey] = undefined;
         }
 
-        var aValue = a[aKey]
-        var bValue = b[aKey]
-
+        var aValue = a[aKey];
+        var bValue = b[aKey];
+        // 相等的部分跳过
         if (aValue === bValue) {
-            continue
+            continue;
         } else if (isObject(aValue) && isObject(bValue)) {
             if (getPrototype(bValue) !== getPrototype(aValue)) {
-                diff = diff || {}
-                diff[aKey] = bValue
+                diff = diff || {};
+                diff[aKey] = bValue;
             } else if (isHook(bValue)) {
-                 diff = diff || {}
-                 diff[aKey] = bValue
+                // 函数直接回调？
+                diff = diff || {};
+                diff[aKey] = bValue;
             } else {
-                var objectDiff = diffProps(aValue, bValue)
+                // 递归检查，是否存在相同的
+                var objectDiff = diffProps(aValue, bValue);
                 if (objectDiff) {
-                    diff = diff || {}
-                    diff[aKey] = objectDiff
+                    diff = diff || {};
+                    diff[aKey] = objectDiff;
                 }
             }
         } else {
-            diff = diff || {}
-            diff[aKey] = bValue
+            diff = diff || {};
+            diff[aKey] = bValue;
         }
     }
 
+    // 将剩下的目标的属性 copy 到目标中
     for (var bKey in b) {
         if (!(bKey in a)) {
-            diff = diff || {}
-            diff[bKey] = b[bKey]
+            diff = diff || {};
+            diff[bKey] = b[bKey];
         }
     }
 
-    return diff
+    return diff;
 }
 
 function getPrototype(value) {
-  if (Object.getPrototypeOf) {
-    return Object.getPrototypeOf(value)
-  } else if (value.__proto__) {
-    return value.__proto__
-  } else if (value.constructor) {
-    return value.constructor.prototype
-  }
+    if (Object.getPrototypeOf) {
+        return Object.getPrototypeOf(value);
+    } else if (value.__proto__) {
+        return value.__proto__;
+    } else if (value.constructor) {
+        return value.constructor.prototype;
+    }
 }
